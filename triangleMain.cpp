aa755bf22b3485df347713d88472242ddc738e09
miles <miles@coders-of-the-caribbean.org>
2018-09-17 15:15:52 +0200

    Opening a window

--- triangleMain.cpp
+++ triangleMain.cpp
@@ -1,38 +1,76 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
 
-class HelloTriangleApplication {
+
+const int WIDTH   = 800;
+const int HEIGHT  = 600;
+const char* TITLE = "Vulkan";
+
+class HelloTriangleApplication
+{
 public:
-    void run() {
+    HelloTriangleApplication()
+    {
+        initWindow();
         initVulkan();
-        mainLoop();
+    }
+
+    ~HelloTriangleApplication()
+    {
         cleanup();
     }
 
+    void run()
+    {
+        mainLoop();
+    }
+
 private:
-    void initVulkan() {
+    void initWindow()
+    {
+        glfwInit();
 
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, nullptr, nullptr);
     }
 
-    void mainLoop() {
-
+    void initVulkan()
+    {
     }
 
-    void cleanup() {
-
+    void mainLoop()
+    {
+        while(!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
+
+    void cleanup()
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+
+    GLFWwindow* window = nullptr;
 };
 
-int main() {
+int main()
+{
     HelloTriangleApplication app;
 
-    try {
+    try
+    {
         app.run();
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception& e)
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }

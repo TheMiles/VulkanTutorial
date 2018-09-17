41afb5bd3c191fba21a9cad01fec00c3d5b9b222
miles <miles@coders-of-the-caribbean.org>
2018-09-17 17:23:41 +0200

    Adding createInstance method

--- triangleMain.cpp
+++ triangleMain.cpp
@@ -5,6 +5,7 @@
 #include <stdexcept>
 #include <functional>
 #include <cstdlib>
+#include <vector>
 
 
 const int WIDTH   = 800;
@@ -17,6 +18,7 @@ public:
     HelloTriangleApplication()
     {
         initWindow();
+        enumerateExtensions();
         initVulkan();
     }
 
@@ -42,6 +44,7 @@ private:
 
     void initVulkan()
     {
+        createInstance();
     }
 
     void mainLoop()
@@ -54,11 +57,55 @@ private:
 
     void cleanup()
     {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
 
+    void createInstance()
+    {
+        VkApplicationInfo appInfo = {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        VkInstanceCreateInfo createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        if( vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS )
+        {
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
+
+    void enumerateExtensions()
+    {
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:" << std::endl;
+        for (const auto& extension : extensions)
+        {
+            std::cout << "\t" << extension.extensionName << std::endl;
+        }
+    }
+
     GLFWwindow* window = nullptr;
+    VkInstance  instance;
 };
 
 int main()

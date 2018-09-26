02d743a78bb4d8b1090eec16d69d20ed732c693e
miles <miles@coders-of-the-caribbean.org>
2018-09-26 15:13:39 +0200

    Creating RenderPass

--- triangleMain.cpp
+++ triangleMain.cpp
@@ -108,6 +108,7 @@ private:
         createLogicalDevice();
         createSwapChain();
         createImageViews();
+        createRenderPass();
     }
 
     void mainLoop()
@@ -120,6 +121,8 @@ private:
 
     void cleanup()
     {
+        vkDestroyRenderPass(device, renderPass, nullptr);
+
         for (auto imageView : swapChainImageViews)
         {
             vkDestroyImageView(device, imageView, nullptr);
@@ -630,6 +633,40 @@ private:
         }
     }
 
+    void createRenderPass()
+    {
+        VkAttachmentDescription colorAttachment  = {};
+        colorAttachment.format                   = swapChainImageFormat;
+        colorAttachment.samples                  = VK_SAMPLE_COUNT_1_BIT;
+        colorAttachment.loadOp                   = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp                  = VK_ATTACHMENT_STORE_OP_STORE;
+        colorAttachment.stencilLoadOp            = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        colorAttachment.stencilStoreOp           = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        colorAttachment.initialLayout            = VK_IMAGE_LAYOUT_UNDEFINED;
+        colorAttachment.finalLayout              = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        VkAttachmentReference colorAttachmentRef = {};
+        colorAttachmentRef.attachment            = 0;
+        colorAttachmentRef.layout                = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass             = {};
+        subpass.pipelineBindPoint                = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount             = 1;
+        subpass.pColorAttachments                = &colorAttachmentRef;
+
+        VkRenderPassCreateInfo renderPassInfo    = {};
+        renderPassInfo.sType                     = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        renderPassInfo.attachmentCount           = 1;
+        renderPassInfo.pAttachments              = &colorAttachment;
+        renderPassInfo.subpassCount              = 1;
+        renderPassInfo.pSubpasses                = &subpass;
+
+        if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create render pass!");
+        }
+    }
+
 
     GLFWwindow*              window         = nullptr;
     VkInstance               instance;
@@ -644,6 +681,7 @@ private:
     VkFormat                 swapChainImageFormat;
     VkExtent2D               swapChainExtent;
     std::vector<VkImageView> swapChainImageViews;
+    VkRenderPass             renderPass;
 };
 
 int main()

845900d49314b06c4c43778d1b1e9d2839b34778
miles <miles@coders-of-the-caribbean.org>
2018-09-26 16:35:00 +0200

    creating Framebuffer

--- triangleMain.cpp
+++ triangleMain.cpp
@@ -111,6 +111,7 @@ private:
         createImageViews();
         createRenderPass();
         createGraphicsPipeline();
+        createFramebuffers();
     }
 
     void mainLoop()
@@ -123,6 +124,11 @@ private:
 
     void cleanup()
     {
+        for (auto framebuffer : swapChainFramebuffers)
+        {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
+
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
@@ -840,22 +846,49 @@ private:
         }
     }
 
-    GLFWwindow*              window         = nullptr;
-    VkInstance               instance;
-    VkDebugReportCallbackEXT callback;
-    VkSurfaceKHR             surface;
-    VkPhysicalDevice         physicalDevice = VK_NULL_HANDLE;
-    VkDevice                 device;
-    VkQueue                  graphicsQueue;
-    VkQueue                  presentQueue;
-    VkSwapchainKHR           swapChain;
-    std::vector<VkImage>     swapChainImages;
-    VkFormat                 swapChainImageFormat;
-    VkExtent2D               swapChainExtent;
-    std::vector<VkImageView> swapChainImageViews;
-    VkRenderPass             renderPass;
-    VkPipelineLayout         pipelineLayout;
-    VkPipeline               graphicsPipeline;
+    void createFramebuffers()
+    {
+        swapChainFramebuffers.resize(swapChainImageViews.size());
+        for (size_t i = 0; i < swapChainImageViews.size(); i++)
+        {
+            VkImageView attachments[] =
+            {
+                swapChainImageViews[i]
+            };
+
+            VkFramebufferCreateInfo framebufferInfo = {};
+            framebufferInfo.sType                   = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass              = renderPass;
+            framebufferInfo.attachmentCount         = 1;
+            framebufferInfo.pAttachments            = attachments;
+            framebufferInfo.width                   = swapChainExtent.width;
+            framebufferInfo.height                  = swapChainExtent.height;
+            framebufferInfo.layers                  = 1;
+
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapChainFramebuffers[i]) != VK_SUCCESS)
+            {
+                throw std::runtime_error("failed to create framebuffer!");
+            }
+        }
+    }
+
+    GLFWwindow*                window         = nullptr;
+    VkInstance                 instance;
+    VkDebugReportCallbackEXT   callback;
+    VkSurfaceKHR               surface;
+    VkPhysicalDevice           physicalDevice = VK_NULL_HANDLE;
+    VkDevice                   device;
+    VkQueue                    graphicsQueue;
+    VkQueue                    presentQueue;
+    VkSwapchainKHR             swapChain;
+    std::vector<VkImage>       swapChainImages;
+    VkFormat                   swapChainImageFormat;
+    VkExtent2D                 swapChainExtent;
+    std::vector<VkImageView>   swapChainImageViews;
+    VkRenderPass               renderPass;
+    VkPipelineLayout           pipelineLayout;
+    VkPipeline                 graphicsPipeline;
+    std::vector<VkFramebuffer> swapChainFramebuffers;
 };
 
 int main()

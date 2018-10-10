107a6f180449ee54cc0c29453265a6cf385407a9
miles <miles@coders-of-the-caribbean.org>
2018-10-10 15:46:46 +0200

    Fixing resize bug
    The resized flag has never been reset. This lead to recreation of
    the swapchain every frame.
    Inside the recreateSwapChain() the glfwWaitEvents() is triggered
    which is empty most of the times. So the redraw hangs until
    some event is happening.

--- triangleMain.cpp
+++ triangleMain.cpp
@@ -1953,6 +1953,7 @@ private:
 
         if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || framebufferResized)
         {
+            framebufferResized = false;
             recreateSwapChain();
         }
         else if (result != VK_SUCCESS)
